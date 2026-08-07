5c4fbb1781b5e7f16dcaae0d8c6a0eedb2c959e5
fridrikur <fridrikur@gmail.com>
2026-08-07 20:02:05 +0200

    Backup af projektkildekode

--- config/filesystems.php
+++ config/filesystems.php
@@ -87,4 +87,11 @@ return [
         public_path('storage') => storage_path('app/public'),
     ],
 
+    'google' => [
+        'driver' => 'google',
+        'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+        'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+        'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+        'folder' => env('GOOGLE_DRIVE_FOLDER'), // F.eks. 'LaravelBackups'
+    ],
 ];
