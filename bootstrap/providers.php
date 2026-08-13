a77f175ca6084957c98bf642dfa6382c9ceb3bd9
fridrikur <fridrikur@gmail.com>
2026-08-13 23:25:58 +0200

    Ret stavemåde fra liveWire til livewire/Livewire

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,4 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    Livewire\LivewireServiceProvider::class, // <--- Tilføj denne med stort W
 ];
