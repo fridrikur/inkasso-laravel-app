4b41cb9722890d6aa6f604c1254cbef565cd7746
fridrikur <fridrikur@gmail.com>
2026-08-14 10:09:32 +0200

    fjernede jetstream fra providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     // App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
+    // App\Providers\JetstreamServiceProvider::class,
 ];
