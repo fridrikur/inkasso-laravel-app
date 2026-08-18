1f2179a8147a6678d3432e09a83d9942b583aff0
fridrikur <fridrikur@gmail.com>
2026-08-18 23:28:44 +0200

    2fa

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,8 +68,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'two_factor_secret' => 'encrypted', // Add this line
+            'password' => 'hashed',            
         ];
     }
 
