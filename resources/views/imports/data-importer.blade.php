864a35f8dd6bd5ad795ff5279f2a3fdc7cd2a311
fridrikur <fridrikur@gmail.com>
2026-09-08 23:08:32 +0200

    data import

--- resources/views/imports/data-importer.blade.php
+++ resources/views/imports/data-importer.blade.php
@@ -403,7 +403,7 @@ Visse felter håndteres nu via avancerede relationer (f.eks. `sager_konsulent`, 
                         </button>
                         
                         {{-- LUKKER MODALEN OG KALDER DEN SYNKRONE METODE --}}
-                        <button type="button" @click="showConfirmModal = false; $wire.runDialogImportDirectly()" wire:loading.attr="disabled" class="px-5 py-2 text-xs font-bold bg-indigo-600 hover:bg-indigo-700 text-white rounded-xl shadow-xs transition cursor-pointer">
+                        <button type="button" wire:click="runDialogImportDirectly" @click="showConfirmModal = false" wire:loading.attr="disabled" class="px-5 py-2 text-xs font-bold bg-indigo-600 hover:bg-indigo-700 text-white rounded-xl shadow-xs transition cursor-pointer">
                             <span wire:loading.remove>Ja, start import 🚀</span>
                             <span wire:loading>Importerer data... ⏳</span>
                         </button>
