31b416952bedfee9ad2718b489347c143a80827c
fridrikur <fridrikur@gmail.com>
2026-09-08 23:34:45 +0200

    dialog participant

--- app/Models/DialogMessage.php
+++ app/Models/DialogMessage.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes; // 🟢 Husk denne import
+use App\Models\DialogParticipant;
 
 class DialogMessage extends Model
 {
