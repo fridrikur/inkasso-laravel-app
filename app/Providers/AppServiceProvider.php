9c553baea1f3124b10f47c41435f8f5581b776f0
fridrikur <fridrikur@gmail.com>
2026-09-09 13:45:01 +0200

    performance sager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,6 +36,13 @@ class AppServiceProvider extends ServiceProvider
                 'last_login_browser' => request()->userAgent(),
                 'login_count' => ($user->login_count ?? 0) + 1,
             ])->save();
+        });
+        DB::listen(function ($query) {
+            Log::info('SQL', [
+                'time' => $query->time . ' ms',
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+            ]);
         });
     }
 }
